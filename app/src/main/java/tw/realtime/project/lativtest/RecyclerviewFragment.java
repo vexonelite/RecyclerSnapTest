ab5f61b96c435588bff8b54b7b3d744b59dfa60d
vexonelite <elite@realtime.tw>
2017-05-03 17:11:58 +0800

    remove useless methods; use PagerSnapHelper, alter name of method and field

--- app/src/main/java/tw/realtime/project/lativtest/RecyclerviewFragment.java
+++ app/src/main/java/tw/realtime/project/lativtest/RecyclerviewFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +21,9 @@ import java.util.List;
 public class RecyclerviewFragment extends Fragment {
 
 
-    private int mPosition;
+    private int mBackgroundColor;
 
+    private LinearLayoutManager mLinearLayoutManager;
     private RecyclerView mRecyclerView;
     private AbstractRecycleListAdapter mAdapter;
     //private int mScrollPosition;
@@ -29,15 +32,15 @@ public class RecyclerviewFragment extends Fragment {
         return this.getClass().getSimpleName();
     }
 
-    public void setPosition (int position) {
-        mPosition = position;
+    public void setBackgroundColor (int color) {
+        mBackgroundColor = color;
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
-                             Bundle savedInstanceState) {
+                             @Nullable Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_recyclerview, container, false);
     }
@@ -72,14 +75,22 @@ public class RecyclerviewFragment extends Fragment {
         }
 
         mAdapter = new MyListAdapter();
-        ((MyListAdapter)mAdapter).allNewDataSet(dataList, true);
+        ((MyListAdapter)mAdapter).appendNewDataSet(dataList, true);
+
+        mLinearLayoutManager = new LinearLayoutManager(getContext());
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
         mRecyclerView = recyclerView;
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
         mRecyclerView.setItemAnimator(null);
         mRecyclerView.setAdapter(mAdapter);
-        //recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), recyclerView, this));
-        //optimize();
+
+        //Page mode, but it can turn multiple pages at once when user is flipping
+        //new LinearSnapHelper().attachToRecyclerView(recyclerView);
+
+        // Page mode, but has the same effect as using ViewPager
+        new PagerSnapHelper().attachToRecyclerView(recyclerView);
+
         mRecyclerView.setHasFixedSize(true);
 
     }
@@ -104,13 +115,11 @@ public class RecyclerviewFragment extends Fragment {
 
     private class MyListItem extends RecyclerView.ViewHolder {
 
-        private View mContainer;
         private TextView mLabelView;
 
         private MyListItem (View itemView) {
             super(itemView);
-            mContainer = itemView;
-            mContainer.setBackgroundColor(getColorByPosition());
+            itemView.setBackgroundColor(mBackgroundColor);
             mLabelView = (TextView) itemView.findViewById(R.id.label);
         }
 
@@ -126,90 +135,17 @@ public class RecyclerviewFragment extends Fragment {
         }
 
         private void resetItemViewLooks () {
-            mContainer.setOnClickListener(null);
-            mLabelView.setText("");//
+            mLabelView.setText("");
         }
     }
 
-
-
-
-    /**
-     * ref: https://github.com/nostra13/Android-Universal-Image-Loader/wiki/Useful-Info
-     *
-    private void optimize() {
-
-        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                ImageLoader imageLoader = ImageLoader.getInstance();
-                switch (newState) {
-                    case RecyclerView.SCROLL_STATE_IDLE:
-                        imageLoader.resume();
-                        break;
-                    case RecyclerView.SCROLL_STATE_DRAGGING:
-                        imageLoader.pause();
-                        break;
-                    case RecyclerView.SCROLL_STATE_SETTLING:
-                        imageLoader.pause();
-                        break;
-                }
-            }
-            /*
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                LinearLayoutManager manager = ((LinearLayoutManager)recyclerView.getLayoutManager());
-                boolean enabled = manager.findFirstCompletelyVisibleItemPosition() == 0;
-                //s.setEnabled((ViewCompat.canScrollVertically(recyclerView, -1) ));
-                mSwipeRefresh.setEnabled(enabled);
-            }
-            * /
-        });
-    }
-    */
-
-
-    /*
-    protected void scrollToSpecifiedPosition (int position) {
+    public void scrollToSpecifiedPosition (int position) {
         try {
-            //Log.i(getLogTag(), "scrollToSpecifiedPosition: " + position);
-            //mRecyclerView.scrollToPosition(position);
-            LogWrapper.showLog(Log.INFO, getLogTag(), "scrollToSpecifiedPosition: " + mScrollPosition);
-            mRecyclerView.scrollToPosition(mScrollPosition);
+            LogWrapper.showLog(Log.INFO, getLogTag(), "scrollToSpecifiedPosition: " + position);
+            mRecyclerView.scrollToPosition(position);
         }
         catch (Exception e) {
-            e.printStackTrace();
+            LogWrapper.showLog(Log.ERROR, getLogTag(), "Exception on scrollToSpecifiedPosition!", e);
         }
     }
-
-    protected int getScrollPosition () {
-        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
-        mScrollPosition = layoutManager.findFirstVisibleItemPosition();
-        LogWrapper.showLog(Log.INFO, getLogTag(), "getScrollPosition: " + mScrollPosition);
-        return layoutManager.findFirstCompletelyVisibleItemPosition();
-    }
-    */
-
-    private int getColorByPosition () {
-        int resId = R.color.gray_0004;
-        switch (mPosition) {
-            case 0:
-                resId = R.color.pink_002;
-                break;
-            case 1:
-                resId = R.color.blue_001;
-                break;
-            case 2:
-                resId = R.color.orange_0002;
-                break;
-            case 3:
-                resId = R.color.green_0003;
-                break;
-            case 4:
-                resId = R.color.yellow_0001;
-                break;
-        }
-        return CodeUtils.getColorFromResourceId(getContext(), resId);
-    }
-
 }
